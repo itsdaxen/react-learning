955c6780d5681b33ac34ce66f9f6ee49b558a0bb
itsdaxen <fa.rzam.sadegh@gmail.com>
2025-05-04 11:59:17 +0300

    Refactor App.js: implement step navigation with state management and update button handlers

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,18 +7,22 @@ const messages = [
 ];
 
 export default function App() {
-  let step = 1;
+  const [step, setStep] = useState(1);
 
-  function handlePreviousBtn() {}
+  function handlePreviousBtn() {
+    setStep(step > 1 ? step - 1 : step);
+  }
 
-  function handleNextBtn() {}
+  function handleNextBtn() {
+    setStep(step < 3 ? step + 1 : step);
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">{`Step: ${step}: ${messages[step - 1]}`}</p>
       <div className="buttons">

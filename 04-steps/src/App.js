398bfaa865311bd5a54839ca5874ecc432f7c3bf
itsdaxen <fa.rzam.sadegh@gmail.com>
2025-05-06 09:12:23 +0300

    Enhance App.js: add toggle functionality for step display and improve button handlers for better state management

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -8,37 +8,45 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [winShow, setWinShow] = useState(true);
 
   function handlePreviousBtn() {
-    setStep(step > 1 ? step - 1 : step);
+    setStep(step > 1 ? (step) => step - 1 : step);
   }
 
   function handleNextBtn() {
-    setStep(step < 3 ? step + 1 : step);
+    setStep(step < 3 ? (step) => step + 1 : step);
   }
 
   return (
     <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">{`Step: ${step}: ${messages[step - 1]}`}</p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#ffff" }}
-          onClick={handlePreviousBtn}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#ffff" }}
-          onClick={handleNextBtn}
-        >
-          Next
-        </button>
-      </div>
+      <button onClick={() => setWinShow((is) => !is)} className="close">
+        &times;
+      </button>
+      {winShow ? (
+        <>
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">{`Step: ${step}: ${messages[step - 1]}`}</p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#ffff" }}
+              onClick={handlePreviousBtn}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#ffff" }}
+              onClick={handleNextBtn}
+            >
+              Next
+            </button>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 }

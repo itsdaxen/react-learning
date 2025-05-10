16382f156880718ecbaf3eb652c119c4b7860560
itsdaxen <fa.rzam.sadegh@gmail.com>
2025-05-10 05:44:21 +0300

    Add initial project setup for steps navigation app: include package.json, package-lock.json, .gitignore, README.md, and essential public assets; implement core structure in App.js with step navigation functionality.

---
+++ steps-ref-children-props/src/App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [winShow, setWinShow] = useState(true);
+
+  function handlePreviousBtn() {
+    setStep(step > 1 ? (step) => step - 1 : step);
+  }
+
+  function handleNextBtn() {
+    setStep(step < 3 ? (step) => step + 1 : step);
+  }
+
+  return (
+    <div className="steps">
+      <button onClick={() => setWinShow((is) => !is)} className="close">
+        &times;
+      </button>
+      {winShow ? (
+        <>
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">{`Step: ${step}: ${messages[step - 1]}`}</p>
+          <div className="buttons">
+            <Button btnLabel={"previous"} onClickFunction={handlePreviousBtn} />
+            <Button btnLabel={"next"} onClickFunction={handleNextBtn} />
+          </div>
+        </>
+      ) : null}
+    </div>
+  );
+
+  function Button({ btnLabel, onClickFunction }) {
+    return (
+      <button
+        style={{ backgroundColor: "#7950f2", color: "#ffff" }}
+        onClick={onClickFunction}
+      >
+        {btnLabel}
+      </button>
+    );
+  }
+}
